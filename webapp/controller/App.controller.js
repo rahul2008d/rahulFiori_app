b457647dd2576fc3f3bcbed912746ff1cd541a97
Datta <Rahul.Datta@lamresearch.com>
2022-08-03 13:32:54 +0530

    My First state

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+
+    'lam/fin/ar/controller/BaseController'
+
+], function(BaseController) {
+    'use strict';
+    return BaseController.extend("lam.fin.ar.controller.App",{
+        onInit: function(){ 
+
+        }
+    });
+});
